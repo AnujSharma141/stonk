61b3422b6ffa52206f8ce934fe7cc9d1b4e28077
AnujSharma141 <19ucc109@lnmiit.ac.in>
2021-05-24 10:41:37 +0530

    yargs config

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
-# lyricspotify
-CLI to get lyrics for tracks playing in spotify.
+
+<p align="center">
+    <img width="480" src="https://i.ibb.co/0VSsDzN/image.png" />
+</p>
+
+## :rocket: Install
+
+```bash
+npm i @anujsharma141/lyrics -g
+```
+
+
+## :pushpin: Usage
+
+```bash
+lyrics <song> <artist>
+```
+
+## :wrench: Development
+Run the following commands in terminal to setup the project on your local machine.
+
+```bash 
+git clone https://github.com/AnujSharma141/lyrics.git
+cd lyrics
+npm install
+npm start
+```
+
--- bin/app.js
+++ bin/app.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 
-// const yargs = require('yargs')
+const yargs = require('yargs')
 
 // const usage = "\nUsage: tran <lang_name> sentence to be translated"
 
@@ -15,9 +15,11 @@
 //     .option("n", { alias: "name", describe: "Your name", type: "string", demandOption: true })
 //     .option("s", { alias: "search", describe: "Search term", type: "string" }).argv
 
+yargs.command('lyrics [artist] [track]',()=>{})
+
 import scrape from '../src/scrape'
 
-scrape('playboi-carti', 'sky')
+scrape('youngthug', 'hot')
 .then(data=> data.error.status? console.log(data.error.message)
     :console.log(data.lyrics))
    
